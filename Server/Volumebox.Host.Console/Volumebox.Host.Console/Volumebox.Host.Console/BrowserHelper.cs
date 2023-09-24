2c4d7b0793e37868484f213acec6e64172e9d8ba
whelpy <whelpy@yandex.ru>
2023-09-24 08:45:12 +0300

    Scan active browser processes to get their tab names. Only active tab returns its name...

---
+++ Server/Volumebox.Host.Console/Volumebox.Host.Console/Volumebox.Host.Console/BrowserHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volumebox.Host.Console
+{
+    internal class BrowserHelper
+    {
+        [DllImport("user32.dll")]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        public void PrintBrowserTabName()
+        {
+            var browsersList = new List<string>
+            {
+                "chrome",
+                "firefox",
+                "iexplore",
+                "safari",
+                "opera",
+                "edge"
+            };
+
+            foreach (var singleBrowser in browsersList)
+            {
+                var process = Process.GetProcessesByName(singleBrowser);
+                if (process.Length > 0)
+                {
+                    foreach (Process singleProcess in process)
+                    {
+                        IntPtr hWnd = singleProcess.MainWindowHandle;
+                        int length = GetWindowTextLength(hWnd);
+
+                        StringBuilder text = new StringBuilder(length + 1);
+                        GetWindowText(hWnd, text, text.Capacity);
+                        var title = text.ToString();
+                        if (!String.IsNullOrEmpty(title))
+                            System.Console.WriteLine(text.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
--- Server/Volumebox.Host.Console/Volumebox.Host.Console/Volumebox.Host.Console/Program.cs
+++ Server/Volumebox.Host.Console/Volumebox.Host.Console/Volumebox.Host.Console/Program.cs
@@ -1,22 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Volumebox.Host.Console;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Volumebox.Host.Console");
+
+var browsersList = new List<string>
+    {
+        "chrome",
+        "firefox",
+        "iexplore",
+        "safari",
+        "opera",
+        "edge"
+    };
+
+Console.WriteLine("Active browser tab names:");
+var browserHelper = new BrowserHelper();
+browserHelper.PrintBrowserTabName();
+
+Console.WriteLine("\nProcesses with audio output:");
 
 var worker = new ProcessWorker();
 foreach (string name in worker.EnumerateApplications())
 {
-    Console.WriteLine("Process name: " + name);
-    
-    {
-        // display mute state & volume level (% of master)
-        Console.WriteLine("Mute: " + worker.GetApplicationMute(name));
-        Console.WriteLine("Volume: " + worker.GetApplicationVolume(name));
+    Console.WriteLine("     Process name: " + name);
 
-        // mute the application
-        //SetApplicationMute(app, true);
 
-        // set the volume to half of master volume (50%)
-        //SetApplicationVolume(app, 50);
-    }
+    // display mute state & volume level (% of master)
+    Console.WriteLine("         Mute: " + worker.GetApplicationMute(name));
+    Console.WriteLine("         Volume: " + worker.GetApplicationVolume(name));
+
+
+
+    // mute the application
+    //SetApplicationMute(app, true);
+
+    // set the volume to half of master volume (50%)
+    //SetApplicationVolume(app, 50);
+
 }
