53ea95ec5bef31d1d2aa44ada54038000f97d995
whelpy <whelpy@yandex.ru>
2023-09-24 08:33:30 +0300

    List all windows processes with audio output.

---
+++ Server/Volumebox.Host.Console/Volumebox.Host.Console/Volumebox.Host.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Volumebox.Host.Console;
+
+Console.WriteLine("Hello, World!");
+
+var worker = new ProcessWorker();
+foreach (string name in worker.EnumerateApplications())
+{
+    Console.WriteLine("Process name: " + name);
+    
+    {
+        // display mute state & volume level (% of master)
+        Console.WriteLine("Mute: " + worker.GetApplicationMute(name));
+        Console.WriteLine("Volume: " + worker.GetApplicationVolume(name));
+
+        // mute the application
+        //SetApplicationMute(app, true);
+
+        // set the volume to half of master volume (50%)
+        //SetApplicationVolume(app, 50);
+    }
+}
